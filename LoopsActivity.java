e9bdb8166b7776a91bd94827e2c3480ea0ce27a7
Carwen Barnard <carwen.barnard@gmail.com>
2019-02-08 14:42:50 +1300

    Add files via upload

---
+++ LoopsActivity.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loopsactivity;
+import java.util.*;
+import java.awt.*;
+/**
+ * created 08/02/2019 
+ * @author Carwen
+ */
+public class LoopsActivity {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LoopsForm form = new LoopsForm();
+        form.setVisible(true);
+        
+    }
+    
+}
